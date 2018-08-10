9cdd88094a2fa6056916356e215184c59ca70c0a
Gabriel Guimaraes <gabriellimaguimaraes@gmail.com>
2018-08-09 17:52:19 -0700

    Better defaults and adding onChanges

--- src/pagedraw-specs.js
+++ src/pagedraw-specs.js
@@ -15,45 +15,81 @@ import {
     Radio
 } from '@material-ui/core';
 
-const Enum = (options) => ({__ty: 'Enum', options});
+const Enum = (options, required, defaultValue) => ({type: 'Enum', options, required, defaultValue});
 
 // Material ui types
-const colorTy = Enum(['default', 'primary', 'secondary']);
-const variantTy = Enum(['raised', 'flat', 'outlined', 'fab']);
+const colorTy = Enum(['default', 'primary', 'secondary'], false, 'primary');
+const variantTy = Enum(['raised', 'flat', 'outlined', 'fab'], false, 'raised');
 const sizeTy = Enum(['small', 'medium', 'large']);
 
 // React bootstrap types
 const bsStyleTy = Enum(['success', 'warning', 'danger', 'info']);
 const bsSizeTy = Enum(["large", "small", "xsmall"])
 
-Button.pdPropControls = {'variant': variantTy, size: sizeTy, 'color': colorTy, 'children': 'Text', disabled: 'Boolean', fullWidth: 'Boolean'};
+Button.pdPropControls = {
+    'variant': variantTy,
+    size: sizeTy,
+    'color': colorTy,
+    'children': {type: 'Text', required: true, defaultValue: 'Click me'},
+    disabled: 'Boolean',
+    onClick: {type: 'Function'}
+};
 Button.pdResizable = ['width'];
 
-IconButton.pdPropControls = {color: colorTy, 'children': 'Text', disabled: 'Boolean'}
+IconButton.pdPropControls = {color: colorTy, 'children': {type: 'Text', required: true, defaultValue: 'icon'}, disabled: 'Boolean'}
 IconButton.pdResizable = ['width']
 
-Switch.pdPropControls = {color: colorTy, checked: 'Boolean', disabled: 'Boolean'}
+Switch.pdPropControls = {
+    color: colorTy, checked: 'Boolean', disabled: 'Boolean',
+    onChange: {type: 'Function'}
+}
 Switch.pdResizable = [];
 
-TextField.pdPropControls = {defaultValue: 'Text', helperText: 'Text', label: 'Text', disabled: 'Boolean', error: 'Boolean', fullWidth: 'Boolean'};
+TextField.pdPropControls = {
+    defaultValue: 'Text',
+    helperText: 'Text',
+    label: {type: 'Text', defaultValue: 'Username'},
+    value: {type: 'Text', defaultValue: ''},
+    onChange: {type: 'Function'},
+    disabled: 'Boolean',
+    error: 'Boolean'
+};
 TextField.pdResizable = ['width'];
 
-Checkbox.pdPropControls = {color: colorTy, checked: 'Boolean', disabled: 'Boolean'};
+Checkbox.pdPropControls = {
+    color: colorTy, checked: 'Boolean', disabled: 'Boolean',
+    onChange: {type: 'Function'}
+};
 Checkbox.pdResizable = [];
 
-CircularProgress.pdPropControls = {color: colorTy, thickness: 'Number', size: 'Number', value: "Number", variant: Enum(['determinate', 'indeterminate', 'static'])}
+CircularProgress.pdPropControls = {
+    color: colorTy,
+    thickness: 'Number',
+    size: {type: 'Number', defaultValue: 40}, value: "Number",
+    variant: Enum(['determinate', 'indeterminate', 'static'], false, 'indeterminate')
+}
 CircularProgress.pdResizable = []
 
-Input.pdPropControls = {defaultValue: 'Text', disabled: "Boolean", disableUnderline: "Boolean", error: 'Boolean', multiline: 'Boolean'}
+Input.pdPropControls = {
+    defaultValue: 'Text', disabled: "Boolean", disableUnderline: "Boolean", error: 'Boolean', multiline: 'Boolean',
+    value: {type: 'Text', defaultValue: ''},
+    placeholder: {type: 'Text', defaultValue: 'Email'},
+    required: 'Checkbox',
+    onChange: 'Function'
+}
 Input.pdResizable = ['width'];
 
-LinearProgress.pdPropControls = {value: 'Number', valueBuffer: 'Number', color: Enum(['primary', 'secondary']), variant: Enum(['determinate', 'indeterminate', 'buffer', 'query'])};
+LinearProgress.pdPropControls = {
+    value: {type: 'Number', defaultValue: 50},
+    valueBuffer: 'Number', color: Enum(['primary', 'secondary']),
+    variant: Enum(['determinate', 'indeterminate', 'buffer', 'query'])
+};
 LinearProgress.pdResizable = ['width'];
 
 /*Select.pdPropControls = {value: 'Text', open: 'Boolean', displayEmpty: 'Boolean', children: 'Text'};
 Select.pdResizable = ['width']; Bugs the store for now */
 
-Radio.pdPropControls = {color: colorTy, disabled: 'Boolean', disableRipple: 'Boolean', checked: 'Boolean'}
+Radio.pdPropControls = {color: colorTy, disabled: 'Boolean', disableRipple: 'Boolean', checked: 'Boolean', onChange: {type: 'Function'}}
 Radio.pdResizable = []
 
 export default {
